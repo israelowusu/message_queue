84dba468fcf5d9f56616701d61d7599d0ed6f7e2
israelowusu <oaisrael633@gmail.com>
2025-06-26 22:13:39 +0100

    implemented storage

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/israelowusu/message_queue.git
+
+go 1.24.4
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+// Underlying storage (in memory, on disk, s3, anything)
+// server (http, tcp, grpc)
+
+func main() {
+	fmt.Println("hello ")
+}
